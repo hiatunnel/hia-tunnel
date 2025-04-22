8de80a5ef95671c74809c11e4b25877fa6dafc9c
Administrator <scrmsmart@gmail.com>
2025-04-22 15:21:40 +0800

    fix mux.go

--- internal/mux/mux.go
+++ internal/mux/mux.go
@@ -1,14 +1,27 @@
 package mux
-import ("github.com/xtaci/smux";"io";"net")
-func Server(c io.ReadWriteCloser,max int)(*smux.Session,error){
-    cfg:=smux.DefaultConfig();cfg.MaxStreamBufferSize=256<<10;cfg.MaxStreamsInWindow=max
-    return smux.Server(c,cfg)
+
+import (
+	"github.com/xtaci/smux"
+	"io"
+	"net"
+)
+
+func Server(c io.ReadWriteCloser, max int) (*smux.Session, error) {
+	cfg := smux.DefaultConfig()
+	return smux.Server(c, cfg)
 }
-func Client(c io.ReadWriteCloser,max int)(*smux.Session,error){
-    cfg:=smux.DefaultConfig();cfg.MaxStreamBufferSize=256<<10;cfg.MaxStreamsInWindow=max
-    return smux.Client(c,cfg)
+
+func Client(c io.ReadWriteCloser, max int) (*smux.Session, error) {
+	cfg := smux.DefaultConfig()
+	return smux.Client(c, cfg)
 }
-func Pipe(s *smux.Stream,target string){
-    dst,err:=net.Dial("tcp",target);if err!=nil{_ = s.Close();return}
-    go io.Copy(dst,s);io.Copy(s,dst)
+
+func Pipe(s *smux.Stream, target string) {
+	dst, err := net.Dial("tcp", target)
+	if err != nil {
+		_ = s.Close()
+		return
+	}
+	go io.Copy(dst, s)
+	io.Copy(s, dst)
 }
