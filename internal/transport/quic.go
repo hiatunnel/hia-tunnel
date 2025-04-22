f0d8db804f2177b5b9c21db2d4ce2879a720ce96
Administrator <scrmsmart@gmail.com>
2025-04-22 15:39:52 +0800

    fix

--- internal/transport/quic.go
+++ internal/transport/quic.go
@@ -7,13 +7,13 @@ import (
 	"github.com/quic-go/quic-go"
 )
 
-func QuicServer(addr string) (quic.Listener, error) {
-	return quic.ListenAddr(addr, ServerTLSConfig(), &quic.Config{
-		EnableDatagrams: true,
-		// v0.25.0 无 KeepAlive 字段，也不需要手动设置超时（使用默认即可）
-		MaxIdleTimeout: 30 * time.Second,
-	})
+func QuicServer(addr string) (*quic.Listener, error) {
+    return quic.ListenAddr(addr, ServerTLSConfig(), &quic.Config{
+        EnableDatagrams: true,
+        MaxIdleTimeout:  30 * time.Second,
+    })
 }
+
 
 func QuicDial(addr, sni string) (quic.Connection, error) {
 	return quic.DialAddr(
