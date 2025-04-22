e1f92e1c335ab33964afa0b00c0016df5ecfab2b
Administrator <scrmsmart@gmail.com>
2025-04-22 15:44:10 +0800

    fix

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,24 +1,82 @@
 package main
-import("flag";"io";"log";"net";
-    "github.com/armon/go-socks5";
-    "github.com/hiatunnel/hia-tunnel/internal/config";
-    "github.com/hiatunnel/hia-tunnel/internal/mux";
-    "github.com/hiatunnel/hia-tunnel/internal/transport")
-func main(){
-    cfg:=flag.String("c","./client.json","config");flag.Parse()
-    var cc config.ClientConf
-    if err:=config.Load(*cfg,&cc);err!=nil{log.Fatal(err)}
-    for _,p:=range cc.Peers{go dial(p)}
-    select{}
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"net"
+
+	"github.com/armon/go-socks5"
+	"github.com/hiatunnel/hia-tunnel/internal/config"
+	"github.com/hiatunnel/hia-tunnel/internal/mux"
+	"github.com/hiatunnel/hia-tunnel/internal/transport"
+)
+
+func main() {
+	cfg := flag.String("c", "./client.json", "config")
+	flag.Parse()
+
+	var cc config.ClientConf
+	if err := config.Load(*cfg, &cc); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, p := range cc.Peers {
+		go dial(p)
+	}
+
+	select {}
 }
-func dial(p config.Peer){
-    qc,err:=transport.QuicDial(p.Server,"cdn.cloudflare.com");if err!=nil{log.Println(err);return}
-    sec,_:=transport.WrapNoiseClient(qc.Stream(0),p.PSK)
-    sess,_:=mux.Client(sec,p.MaxStreams)
-    dialf:=func(_,addr string)(net.Conn,error){
-        st,err:=sess.OpenStream();if err!=nil{return nil,err}
-        st.Write([]byte(addr+"\n"));return st,nil}
-    s,_:=socks5.New(&socks5.Config{Dial:dialf})
-    go s.ListenAndServe("tcp",p.SocksLocal)
-    log.Printf("[%s] socks %s -> %s",p.Name,p.SocksLocal,p.Server)
+
+func dial(p config.Peer) {
+	qc, err := transport.QuicDial(p.Server, "cdn.cloudflare.com")
+	if err != nil {
+		log.Printf("[%s] quic dial error: %v", p.Name, err)
+		return
+	}
+
+	stream, err := qc.OpenStreamSync(context.Background())
+	if err != nil {
+		log.Printf("[%s] open stream error: %v", p.Name, err)
+		return
+	}
+
+	sec, err := transport.WrapNoiseClient(stream, p.PSK)
+	if err != nil {
+		log.Printf("[%s] noise handshake error: %v", p.Name, err)
+		return
+	}
+
+	sess, err := mux.Client(sec, p.MaxStreams)
+	if err != nil {
+		log.Printf("[%s] mux error: %v", p.Name, err)
+		return
+	}
+
+	dialf := func(_, addr string) (net.Conn, error) {
+		st, err := sess.OpenStream()
+		if err != nil {
+			return nil, err
+		}
+		_, err = st.Write([]byte(addr + "\n"))
+		if err != nil {
+			return nil, err
+		}
+		return st, nil
+	}
+
+	s, err := socks5.New(&socks5.Config{Dial: dialf})
+	if err != nil {
+		log.Printf("[%s] socks5 init error: %v", p.Name, err)
+		return
+	}
+
+	go func() {
+		if err := s.ListenAndServe("tcp", p.SocksLocal); err != nil {
+			log.Printf("[%s] socks5 error: %v", p.Name, err)
+		}
+	}()
+
+	log.Printf("[%s] socks %s -> %s", p.Name, p.SocksLocal, p.Server)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,33 +1,101 @@
 package main
-import("flag";"io";"log";"net";
-    "github.com/hiatunnel/hia-tunnel/internal/config";
-    "github.com/hiatunnel/hia-tunnel/internal/mux";
-    "github.com/hiatunnel/hia-tunnel/internal/transport")
-func main(){
-    cfgFile:=flag.String("c","/etc/swift-tunnel/server.json","config");flag.Parse()
-    var sc config.ServerConf
-    if err:=config.Load(*cfgFile,&sc);err!=nil{log.Fatal(err)}
-    go forwards(sc.Forwards)
-    lis,err:=transport.QuicServer(sc.Listen);if err!=nil{log.Fatal(err)}
-    log.Printf("[server] quic %s",sc.Listen)
-    for{qc,err:=lis.Accept();if err!=nil{log.Println(err);continue}
-        go handle(qc,&sc)}
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"net"
+
+	"github.com/hiatunnel/hia-tunnel/internal/config"
+	"github.com/hiatunnel/hia-tunnel/internal/mux"
+	"github.com/hiatunnel/hia-tunnel/internal/transport"
+	"github.com/quic-go/quic-go"
+)
+
+func main() {
+	cfgFile := flag.String("c", "/etc/swift-tunnel/server.json", "config")
+	flag.Parse()
+
+	var sc config.ServerConf
+	if err := config.Load(*cfgFile, &sc); err != nil {
+		log.Fatal(err)
+	}
+
+	go forwards(sc.Forwards)
+
+	lis, err := transport.QuicServer(sc.Listen)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("[server] quic %s", sc.Listen)
+
+	for {
+		qc, err := lis.Accept(context.Background())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(qc, &sc)
+	}
 }
-func handle(q transport.QuicConn,sc *config.ServerConf){
-    sec,_:=transport.WrapNoiseServer(q.Stream(0),sc.PSK)
-    sess,_:=mux.Server(sec,1024)
-    for{st,err:=sess.AcceptStream();if err!=nil{return}
-        buf:=make([]byte,256);n,_:=st.Read(buf)
-        go mux.Pipe(st,string(buf[:n-1]))}
+
+func handle(q quic.Connection, sc *config.ServerConf) {
+	stream, err := q.AcceptStream(context.Background())
+	if err != nil {
+		log.Println("stream error:", err)
+		return
+	}
+
+	sec, err := transport.WrapNoiseServer(stream, sc.PSK)
+	if err != nil {
+		log.Println("noise handshake failed:", err)
+		return
+	}
+
+	sess, err := mux.Server(sec, 1024)
+	if err != nil {
+		log.Println("mux server error:", err)
+		return
+	}
+
+	for {
+		st, err := sess.AcceptStream()
+		if err != nil {
+			return
+		}
+		buf := make([]byte, 256)
+		n, _ := st.Read(buf)
+		go mux.Pipe(st, string(buf[:n-1]))
+	}
 }
-func forwards(fs []config.Forward){
-    for _,f:=range fs{go func(f config.Forward){
-        l,err:=net.Listen("tcp",f.Listen);if err!=nil{return}
-        log.Printf("[fwd] %s -> %s",f.Listen,f.Target)
-        for{c,_:=l.Accept();go proxy(c,f.Target)}
-    }(f)}
+
+func forwards(fs []config.Forward) {
+	for _, f := range fs {
+		go func(f config.Forward) {
+			l, err := net.Listen("tcp", f.Listen)
+			if err != nil {
+				log.Printf("listen error: %s -> %s: %v", f.Listen, f.Target, err)
+				return
+			}
+			log.Printf("[fwd] %s -> %s", f.Listen, f.Target)
+			for {
+				c, err := l.Accept()
+				if err != nil {
+					continue
+				}
+				go proxy(c, f.Target)
+			}
+		}(f)
+	}
 }
-func proxy(src net.Conn,target string){
-    dst,err:=net.Dial("tcp",target);if err!=nil{src.Close();return}
-    go io.Copy(dst,src);io.Copy(src,dst)
+
+func proxy(src net.Conn, target string) {
+	dst, err := net.Dial("tcp", target)
+	if err != nil {
+		src.Close()
+		return
+	}
+	go io.Copy(dst, src)
+	io.Copy(src, dst)
 }
